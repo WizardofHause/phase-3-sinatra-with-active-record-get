b410658770804c9f80d706d68c7113f6952e0053
WizardofHause <jonrhause@gmail.com>
2022-11-12 17:40:13 -0700

    commit tree 1d834dc4ad20d11519af0ac67b5c371f3ba15136

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,32 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
   end
 
+  # retrieve ALL games...
+  # get '/games_all' do
+  #   games = Game.all
+  #   games.to_json
+  # end
+
+  # sort games by TITLE...
+  # get '/games_title' do
+  #   games = Game.all.order(:title)
+  #   games.to_json
+  # end
+
+  # same as above, but return ONLY the first 10 games...
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    games = Game.find(params[:id])
+    games.to_json(include: { reviews: { include: :user } })
+  end
+                       
 end
